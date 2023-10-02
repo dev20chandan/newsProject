d564d7ad9d5c70515b13d9c1fae38a99d0afdf4b
Chandan sharma <schandan808.cs@gmail.com>
2023-10-02 11:48:05 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const commonsrc = require('./backend/router/common')
+
+// Serve static files from the 'build' folder
+app.use(express.static(path.join(__dirname, 'build')));
+app.use("/", commonsrc);
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+// Start the server on port 3000
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
