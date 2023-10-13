c1e70b7575b6b13b937dbe141b4ed6cf7fe2fd22
chandan sharma <chandank.sharma@suffescom.com>
2023-10-13 14:04:48 +0530

    add changes for routing

--- news/src/App.js
+++ news/src/App.js
@@ -6,27 +6,46 @@ import AddFeed from './Components/Dashboard/Add';
 import Add from "./Components/Users/Add";
 import UserList from "./Components/Users/UserList";
 
+const routes = [
+  { path: 'Feed', element: <Dashboard />, children: ['add', 'edit', 'view'] },
+  { path: 'userlist', element: <UserList />, children: ['addAdd'] },
+];
+
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/" element={<MainLayout />}>
-            <Route index element={<Dashboard />} /> {/* Show Dashboard by default */}
-            <Route path="Feed" element={<Outlet />}>
-              <Route index element={<Dashboard />} /> {/* Show Dashboard within Feed section */}
-              <Route path="add" element={<AddFeed />} /> {/* Show AddFeed within Feed section */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Dashboard />} />
+          {routes.map((route) => (
+            <Route key={route.path} path={route.path} element={<Outlet />}>
+              <Route index element={route.element} />
+              {route.children.map((child) => (
+                <Route key={child} path={child} element={getRouteComponent(child)} />
+              ))}
             </Route>
-            <Route path="userlist" element={<Outlet />}>
-              <Route index element={<UserList />} /> {/* Show UserList within userlist section */}
-              <Route path="Add" element={<Add />} /> {/* Show Add within userlist section */}
-            </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+          ))}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
+}
+
+function getRouteComponent(child) {
+  // Define a function to return the appropriate component based on the child route
+  switch (child) {
+    case 'add':
+      return <AddFeed />;
+    case 'edit':
+      return <AddFeed />;
+    case 'view':
+      return <AddFeed />;
+    case 'addAdd':
+      return <Add />;
+    default:
+      return null;
+  }
 }
 
 export default App;
