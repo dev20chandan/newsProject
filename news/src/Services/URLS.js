84b72fbd23e28cc062af48fd0cd6d6768c0d52dc
unknown <dev20chandan>
2024-06-25 23:35:29 +0530

    chage url

--- news/src/Services/URLS.js
+++ news/src/Services/URLS.js
@@ -1,4 +1,6 @@
-export const API_URL = "http://localhost:4000";
+// export const API_URL = "http://localhost:4000";
+export const API_URL = "https://newsproject-1.onrender.com";
+
 // export const API_URL = "https://09f35fd8-4000.inc1.devtunnels.ms";
 
 export const Login ="api/loginUser";
