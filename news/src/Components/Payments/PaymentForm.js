89eb918b8d5cb25f9a03be36f91283a0d9f66628
developer2 <phpdev074@gmail.com>
2024-07-21 18:48:10 +0530

    add changes for url payment

--- news/src/Components/Payments/PaymentForm.js
+++ news/src/Components/Payments/PaymentForm.js
@@ -35,47 +35,6 @@ const PaymentForm = () => {
     const order = await response.json();
     console.log(order, '=======oirder');
 
-    // var options = {
-    //   key: "rzp_test_QypuRVCy3b0nhq", // Enter the Key ID generated from the Dashboard
-    //   amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-    //   currency,
-    //   name: "Acme Corp", //your business name
-    //   description: "Test Transaction",
-    //   image: "https://example.com/your_logo",
-    //   order_id: order.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-    //  "handler": function (response){
-    //     console.log(response,'====response')
-    //     alert(response.razorpay_payment_id);
-    //     alert(response.razorpay_order_id);
-    //     alert(response.razorpay_signature)
-    // },
-    //   prefill: {
-    //     //We recommend using the prefill parameter to auto-fill customer's contact information, especially their phone number
-    //     name: "Web Dev Matrix", //your customer's name
-    //     email: "webdevmatrix@example.com",
-    //     contact: "9000000000", //Provide the customer's phone number for better conversion rates
-    //   },
-    //   notes: {
-    //     address: "Razorpay Corporate Office",
-    //   },
-    //   theme: {
-    //     color: "#3399cc",
-    //   },
-    // };
-
-
-    //     razorpay_order_id
-    // : 
-    // "order_ObEMdKvA4qIx2v"
-    // razorpay_payment_id
-    // : 
-    // "pay_ObEMxdVqYZH3Zm"
-    // razorpay_signature
-    // : 
-    // "361add62d53da2b201261dbc9e911070bae866aadd6eeba15bb6e9cff680e6b9"
-
-
-
     var options = {
       "key": "rzp_test_QypuRVCy3b0nhq", // Enter the Key ID generated from the Dashboard
       "amount": amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
